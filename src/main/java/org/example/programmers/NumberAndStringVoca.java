3c60e9c92bf59b04d2081e0ae35bf597efaa993b
K-KY <kyuyoungk@naver.com>
2023-08-03 12:53:59 +0900

    프로그래머스 숫자 문자열과 영단어

---
+++ src/main/java/org/example/programmers/NumberAndStringVoca.java
@@ -0,0 +1,20 @@
+package org.example.programmers;
+
+public class NumberAndStringVoca {
+    public static void main(String[] args) {
+        NumberAndStringVocaSolution s = new NumberAndStringVocaSolution();
+        System.out.println(s.solution("zeroone"));
+    }
+}
+
+class NumberAndStringVocaSolution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] numStr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for (int i = 0; i < numStr.length; i++) {
+            s = s.replace(numStr[i], String.valueOf(i));
+        }
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
