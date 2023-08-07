d8a91a5fd86ce735ca4e82ce1a1799e655defbee
K-KY <kyuyoungk@naver.com>
2023-08-07 19:32:13 +0900

    백준 바킹독 기초문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,38 @@ package org.example;
 
 
 import java.io.*;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main{
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder sb;
+        String str = "";
+        for (int i = 1; i <= n; i++) {
+            sb = new StringBuilder();
+            for (int j = 1; j <= n * 2 - i; j++) {
+                if (j < i || j > n * 2 - i) {
+                    sb.append(" ");
+                    continue;
+                }
+
+                sb.append("*");
+
+            }
+            System.out.println(sb);
+            if (i < n) {
+                sb.append("\n");
+                sb.append(str);
+                str = sb.toString();
+            }
+
+        }
+        System.out.println(str);
+    }
+}
+
+/*public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -34,7 +62,7 @@ public class Main{
         }
         System.out.println(str);
     }
-}
+}*/
 /*public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
