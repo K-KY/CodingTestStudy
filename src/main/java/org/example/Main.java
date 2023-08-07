b66ccc169251bd3663036ef958757595d2700a6d
K-KY <kyuyoungk@naver.com>
2023-08-07 17:32:21 +0900

    백준 바킹독 기초문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,37 @@ package org.example;
 import java.io.*;
 import java.util.Scanner;
 import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        StringBuilder sb;
+        String str = "";
+        for (int i = 1; i <= n; i++) {
+            sb = new StringBuilder();
+            for (int j = 1; j <= n * 2; j++) {
+                if (j <= i || j > n * 2 - i) {
+                    sb.append("*");
+                    continue;
+                }
+
+                    sb.append(" ");
+
+            }
+            System.out.println(sb);
+            //반대편에 출력할 결과를 만든다
+            if (i < n) {
+                sb.append("\n");
+                sb.append(str);
+                str = sb.toString();
+            }
+
+        }
+        System.out.println(str);
+    }
+}
 /*public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
