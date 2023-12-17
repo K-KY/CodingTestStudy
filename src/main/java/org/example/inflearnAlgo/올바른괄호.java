a196979a14c0e0c888834e94d90c21ecc649b259
K-KY <kyuyoungk@naver.com>
2023-12-17 23:10:51 +0900

    인프런 입문 자바 알고리즘 문제 풀이 : 올바른 괄호

---
+++ src/main/java/org/example/inflearnAlgo/올바른괄호.java
@@ -0,0 +1,33 @@
+package org.example.inflearnAlgo;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class 올바른괄호 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int open = 0;
+        int close = 0;
+        List<String> brackets = List.of(sc.nextLine().split(""));
+
+        for (int i = 0; i < brackets.size(); i++) {
+            if (brackets.get(i).equals("(")) {
+                open++;
+            }
+            if (brackets.get(i).equals(")")) {
+                close++;
+            }
+            if (close > open) {
+                break;
+            }
+        }
+
+        System.out.println(open);
+        System.out.println(close);
+        if (open != close) {
+            System.out.println("NO");
+        } else {
+            System.out.println("YES");
+        }
+    }
+}
